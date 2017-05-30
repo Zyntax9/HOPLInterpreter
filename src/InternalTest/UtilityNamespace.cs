cc7bac239e41ccaa65a554d22f4b1e96cd181d43
Steffen Holst Larsen <zteffenz@gmail.com>
2017-05-30 23:22:54 +0200

    Forgot utility namespace

---
+++ src/InternalTest/UtilityNamespace.cs
@@ -0,0 +1,26 @@
+﻿using HOPL.Interpreter.Api;
+using HOPL.Interpreter.Api.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalTest
+{
+    class UtilityNamespace : ISuppliedNamespace
+    {
+        public string Name => "Utility";
+
+        [InterpreterFunction]
+        public int Sleep(int milliseconds)
+        {
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(milliseconds));
+                return 0;
+            });
+            t.Wait();
+            return 1;
+        }
+    }
+}
